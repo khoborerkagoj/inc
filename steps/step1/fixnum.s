df3188861f8e9c371ddd9123cf1d2e6926644b34
Khoborerkagoj <ghoshroy+github@gmail.com>
2016-12-25 12:13:35 -0500

    Updated step1, with instructions.

---
+++ steps/step1/fixnum.s
@@ -0,0 +1,8 @@
+	.text
+	.intel_syntax noprefix
+	.globl	_scheme_entry
+_scheme_entry:                          # @scheme_entry
+	mov	eax, 7
+	ret
+
+
---
+++ steps/step1/startup.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int scheme_entry(void);
+int main(int argc, const char *argv[]) {
+    printf("%d\n", scheme_entry());
+    return 0;
+}
